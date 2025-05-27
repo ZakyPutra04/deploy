d5a669bb640df8539b71ef8089b72b8d7d3b7af7
ZakyPutra04 <153355756+ZakyPutra04@users.noreply.github.com>
2025-05-27 15:57:30 +0800

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,55 +2,55 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use App\Models\User;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        if (env('APP_ENV') ==== 'production') {
+            if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
-        }
-        // Gates for student
+    }
         Gate::define("view-student", function (User $user) {
-            return in_array($user->role, ["admin", "guest"]);
+            if ($user->role === "admin" || $user->role === "guest") {
+                return true;
+            }
+            return false;
         });
 
         Gate::define("store-student", function (User $user) {
-            return $user->role === "admin";
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
         });
 
         Gate::define("edit-student", function (User $user) {
-            return $user->role === "admin";
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
         });
 
         Gate::define("destroy-student", function (User $user) {
-            return $user->role === "admin";
-        });
-
-        // Gates for major
-        Gate::define("view-major", function (User $user) {
-            return in_array($user->role, ["admin", "guest"]);
-        });
-
-        Gate::define("store-major", function (User $user) {
-            return $user->role === "admin";
-        });
-
-        Gate::define("edit-major", function (User $user) {
-            return $user->role === "admin";
-        });
-
-        Gate::define("destroy-major", function (User $user) {
-            return $user->role === "admin";
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
         });
     }
 }
